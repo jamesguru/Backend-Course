11c53a8747cc328dfeb4f5542a319f6a8c9138c6
unknown <jameskagunga2022@gmail.com>
2023-04-17 16:18:29 -0700

    added routes

---
+++ API/src/routes/diagnosis.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { createDiagnosis, getDiagnosisForUser, updateDiagnosis } from "../Controllers/diagnosis";
+
+
+const router = Router();
+
+router.post('/',createDiagnosis);
+router.put('/:id',updateDiagnosis);
+router.post('/user',getDiagnosisForUser)
+
+export default router;
