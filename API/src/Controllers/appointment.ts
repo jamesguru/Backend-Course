3042c2b862a0aa17e3a92b0795679840c0d29166
unknown <jameskagunga2022@gmail.com>
2023-04-25 17:36:31 -0700

    added authentication

--- API/src/Controllers/appointment.ts
+++ API/src/Controllers/appointment.ts
@@ -1,6 +1,21 @@
 import {Response,Request} from "express"
+import Connection from "../Helpers/database"
+const db = new Connection();
 
+export const CreateAppointment = async(req:Request,res:Response) =>{
 
-export const CreateAppointment = () =>{
+    const {patient_name,doctor_email,date,patient_email} = req.body;
+
+    try {
+
+        await db.exec('addAppointment',{patient_name,doctor_email,date,patient_email});
+
+        res.status(201).json({message:"appointment has been created successfully"})
+        
+    } catch (error) {
+        
+      res.status(500).json({error:'something went wrong'})
+    }
+
     
 }
